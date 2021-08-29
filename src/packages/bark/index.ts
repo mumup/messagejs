0045e412a13672392f749a97d3c1df69743700b1
Penley <pmumu666@gmail.com>
2021-08-29 21:07:40 +0800

    init

---
+++ src/packages/bark/index.ts
@@ -0,0 +1,114 @@
+import {
+  BarkBaseConfig,
+  BaseResponse,
+  InfoResponse,
+  PushConfig
+} from './types/api'
+import got from 'got/dist/source'
+import { RequestError } from 'got/dist/source'
+import { URL } from 'url'
+import { assignIn } from 'lodash'
+
+class Bark {
+  constructor(config: BarkBaseConfig) {
+    this._config = {
+      serverUrl: ''
+    }
+    assignIn(this._config, config)
+  }
+
+  private _config: BarkBaseConfig
+
+  private getBaseUrl(path: string) {
+    try {
+      const baseUrl = new URL(
+        this._config.deviceKey + path,
+        this._config.serverUrl
+      )
+      return baseUrl.toString()
+    } catch (error) {
+      throw new Error('url error')
+    }
+  }
+
+  public push = (option: PushConfig): Promise<BaseResponse> => {
+    return new Promise((resolve, reject) => {
+      if (
+        option.deviceKey === undefined &&
+        this._config.deviceKey === undefined
+      ) {
+        reject(new Error('deviceKey is required!'))
+      }
+
+      if (option.title === undefined) {
+        reject(new Error('title is required!'))
+      }
+
+      if (option.body === undefined) {
+        reject(new Error('body is required!'))
+      }
+
+      const url = this.getBaseUrl('/push')
+
+      got
+        .post(url, {
+          json: option
+        })
+        .then((res) => {
+          if (res.statusCode === 200) {
+            resolve(JSON.parse(res.body))
+          }
+        })
+        .catch((err: RequestError) => {
+          reject(err)
+        })
+    })
+  }
+
+  public info = (): Promise<InfoResponse> => {
+    return new Promise((resolve, reject) => {
+      got
+        .post(this._config.serverUrl + '/info')
+        .then((res) => {
+          if (res.statusCode === 200) {
+            resolve(JSON.parse(res.body))
+          }
+        })
+        .catch((err: RequestError) => {
+          reject(err)
+        })
+    })
+  }
+
+  public ping = (): Promise<BaseResponse> => {
+    return new Promise((resolve, reject) => {
+      got
+        .post(this._config.serverUrl + '/ping')
+        .then((res) => {
+          if (res.statusCode === 200) {
+            resolve(JSON.parse(res.body))
+          }
+        })
+        .catch((err: RequestError) => {
+          reject(err)
+        })
+    })
+  }
+
+  public healthz = (): Promise<unknown> => {
+    return new Promise((resolve, reject) => {
+      got
+        .post(this._config.serverUrl + '/healthz')
+        .then((res) => {
+          if (res.statusCode === 200) {
+            resolve(JSON.parse(res.body))
+          }
+        })
+        .catch((err: RequestError) => {
+          reject(err)
+        })
+    })
+  }
+}
+
+export default Bark
